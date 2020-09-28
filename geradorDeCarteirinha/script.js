e2bc84eeca5166f34ec123be5a5e755e9e6858bf
jeffersoncostaIfma <jeffersoncosta@acad.ifma.edu.br>
2020-09-28 17:50:26 -0300

    links dos projetos

---
+++ geradorDeCarteirinha/script.js
@@ -0,0 +1,8 @@
+
+var data = new Date();
+var dia     = data.getDate();           
+var dia_sem = data.getDay();            
+var mes     = data.getMonth();       
+var ano2    = data.getYear();           
+var ano4    = data.getFullYear();
+console.log(ano4);
