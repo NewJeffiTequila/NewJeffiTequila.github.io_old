2681a5e4caba7a95c4193db7b198563e2c282a52
Jefferson Costa <48127640+jeffersoncostaIfma@users.noreply.github.com>
2020-09-30 18:54:49 -0300

    Update script.js

--- geradorDeCarteirinha/script.js
+++ geradorDeCarteirinha/script.js
@@ -1,33 +1,33 @@
 function mostrarValor() {
-    // var data = new Date();
-    // var dia     = data.getDate();           
-    // var mes     = data.getMonth();       
-    // var ano    = data.getFullYear();
+    var data = new Date();
+    var dia     = data.getDate();           
+    var mes     = data.getMonth();       
+    var ano    = data.getFullYear();
     
-    // if (mes < 10) {
-    //     dataExpedição = dia+'/0'+mes+'/'+ano;
-    // } else {
-    //     dataExpedição = dia+'/'+mes+'/'+ano;
-    // }
+    if (mes < 10) {
+        dataExpedição = dia+'/0'+mes+'/'+ano;
+    } else {
+        dataExpedição = dia+'/'+mes+'/'+ano;
+    }
 
     
     
-    // name            =  document.getElementById("name").value;
-    // birthday        =  document.getElementById("birthday").value;
-    // data = new Date(birthday);
-    // dataFormatada = data.toLocaleDateString('pt-BR', {timeZone: 'UTC'});
-    // cpf             =  document.getElementById("cpf").value;
-    // rg              =  document.getElementById("rg").value;
-    // instituition    =  document.getElementById("instituition").value;
-    // registry        =  document.getElementById("registry").value;
-    // console.log(name,birthday,cpf,rg,instituition,registry);
-    // $('.cardName').append('<strong>Nome: </strong>'+name+'');
-    // $('.cardBirthday').append('<strong>Data de Nascimento: </strong>'+dataFormatada+'');
-    // $('.cardRegistry').append('<strong>Matricula: </strong>'+registry+'');
-    // $('.cardCpf').append('<strong>CPF: </strong>'+cpf+'');
-    // $('.cardRg').append('<strong>RG: </strong>'+rg+'');
-    // $('.cardInstituition').append('<strong>Instituição: </strong>'+instituition+'');
-    // $('.cardDataExpedicao').append('<strong>Data de Expedição: </strong>'+dataExpedição+'');
+    name            =  document.getElementById("name").value;
+    birthday        =  document.getElementById("birthday").value;
+    data = new Date(birthday);
+    dataFormatada = data.toLocaleDateString('pt-BR', {timeZone: 'UTC'});
+    cpf             =  document.getElementById("cpf").value;
+    rg              =  document.getElementById("rg").value;
+    instituition    =  document.getElementById("instituition").value;
+    registry        =  document.getElementById("registry").value;
+    console.log(name,birthday,cpf,rg,instituition,registry);
+    $('.cardName').append('<strong>Nome: </strong>'+name+'');
+    $('.cardBirthday').append('<strong>Data de Nascimento: </strong>'+dataFormatada+'');
+    $('.cardRegistry').append('<strong>Matricula: </strong>'+registry+'');
+    $('.cardCpf').append('<strong>CPF: </strong>'+cpf+'');
+    $('.cardRg').append('<strong>RG: </strong>'+rg+'');
+    $('.cardInstituition').append('<strong>Instituição: </strong>'+instituition+'');
+    $('.cardDataExpedicao').append('<strong>Data de Expedição: </strong>'+dataExpedição+'');
 
  
     
@@ -38,13 +38,13 @@ document.getElementById("enviar").onclick = function(e) {
     e.preventDefault();
 }
 
-$(function(){
-    $('#photo').change(function(){
-    const file = $(this)[0].files[0];
-        const fileReader = new FileReader();
-        fileReader.onloadend = function(){
-            $('#img').attr('src',fileReader.result);
-        }
-    fileReader.readAsDataURL(file);
-    });
-});
+// $(function(){
+//     $('#photo').change(function(){
+//     const file = $(this)[0].files[0];
+//         const fileReader = new FileReader();
+//         fileReader.onloadend = function(){
+//             $('#img').attr('src',fileReader.result);
+//         }
+//     fileReader.readAsDataURL(file);
+//     });
+// });
