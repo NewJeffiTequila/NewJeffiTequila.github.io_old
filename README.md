8e3e189f384ddc9f4299cde0d131dd16a8775064
NewJeffiTequila <jeffersoncosta@acad.ifma.edu.br>
2021-02-24 14:31:25 -0300

    subindo outra merda

---
+++ README.md
@@ -0,0 +1,8 @@
+POMODORO DOS DEUSES CRIADO POR MIM HEHE
+Copia não comedia!!
+
+# POMODORO DOS DEUSES CRIA POR MIM HEHE
+## Copia não comedia!!
+
+## So tem HTML CSS e JS
+## So fiz porque tava sem fazer nada de madrugada
---
+++ script.js
@@ -0,0 +1,47 @@
+function start() {
+    const startMinutes = 30;
+    let time = startMinutes * 60;
+    const countdown = document.getElementById('time');
+    const interval = setInterval(() => {
+        const minutes = Math.floor(time/60);
+        let seconds = time % 60;
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+        countdown.innerHTML = `${minutes}:${seconds}`;
+        time--;
+        if( minutes === 0 && seconds === '00'){
+        notifyMe()
+        clearInterval(interval);
+        }
+    }, 1000);
+}
+
+function notifyMe() {
+    if (!("Notification" in window)) 
+    {
+        alert("Este browser não suporta notificações de Desktop");
+    }
+    else if (window.Notification&&Notification.permission!=="denied") 
+    {
+        Notification.requestPermission(function (status) {
+            let notification = new Notification('Ta bom por agora Jeff' ,{ body: 'Vai beber agua'});
+        })
+    }
+}
+
+function breakTime() {
+    const startMinutes = 5;
+    let time = startMinutes * 60;
+    const countdown = document.getElementById('time');
+    const interval = setInterval(() => {
+        const minutes = Math.floor(time/60);
+        let seconds = time % 60;
+        seconds = seconds < 10 ? '0' + seconds : seconds;
+        countdown.innerHTML = `${minutes}:${seconds}`;
+        time--;
+        if( minutes === 0 && seconds === '00'){
+        notifyMe()
+        clearInterval(interval);
+        }
+    }, 1000);
+
+}
